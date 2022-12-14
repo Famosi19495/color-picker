3fb82410e28aec47f66af7503820569e69d0582f
Famosi19495 <famosi19495@gmail.com>
2022-12-14 12:49:03 +0100

    creazione color-picker

---
+++ script.js
@@ -0,0 +1,31 @@
+document.querySelector("#red").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#aquamarine").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#blue").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#lightgreen").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#violet").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#yellow").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
